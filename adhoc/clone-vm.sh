b94c10a03baa5d785371b7fe812e574050b69366
Leela Venkaiah G <lgangava@redhat.com>
2020-12-17 13:28:08 +0530

    add support to clone RHEL 8 VMs on RHEL 7 KVM

--- adhoc/clone-vm.sh
+++ adhoc/clone-vm.sh
@@ -4,25 +4,54 @@
 # BEGIN - Existing base image
 # END - Create VMs from base image and write IPs to hostsfile
 
-# $1=base_image; $2=num_disks; $3=size_of_disk; $4=prefix
-# ./clone-vm.sh rhel7-34 7 25G 7-34
+# $1=base_vm; $2=vm_prefix; $3=num_vms; $4=num_disks; $5=disk_size
+# ./clone-vm.sh rhel7-34 7-34 6 7 25G
 
 # Required RPMs on KVM :
 # yum install virt libvirt libvirt-python libvirt-client libguestfs libguestfs-tools
 # yum groupinstall virtualization-client virtualization-platform virtualization-tools
 # yum expect
 
-# Make interface comeup automatically
-# Better run startup.sh in the base image itself
+# IMP: Run below `startup.sh` in the base image before cloning
 cat << 'EOF' > startup.sh
 #!/bin/bash
+
+# Enalble console access for every VM
+systemctl enable serial-getty@ttyS0.service
+systemctl start serial-getty@ttyS0.service
+
+# Make interface comeup automatically
 ver=$(awk -F':' '{print $5}' /etc/system-release-cpe)
 iface=$([[ $ver =~ ^7 ]] && echo eth0 || echo ens3)
 sed -i 's/ONBOOT=no/ONBOOT=yes/' /etc/sysconfig/network-scripts/ifcfg-$iface
-systemctl enable serial-getty@ttyS0.service
-systemctl start serial-getty@ttyS0.service
+
+# virt-sysprep is still leaving some UUIDs
+# Replace UUID of boot partition with actual disk name
+disk=$(df -k /boot --output=source | tail -n 1)
+cp -f /etc/fstab /etc/fstab.uuid
+sed -r "/boot/ s|^UUID=[[:xdigit:]-]+|$disk|" /etc/fstab.uuid > /etc/fstab
+
 EOF
 
+# Script to be run when VMs are booting for the first time
+cat << 'EOF' > change_uuid.sh
+#!/bin/bash
+
+# Set a new UUID for network interface
+ver=$(awk -F':' '{print $5}' /etc/system-release-cpe)
+iface=$([[ $ver =~ ^7 ]] && echo eth0 || echo ens3)
+uuid=$(uuidgen $iface)
+sed -ir "s/^UUID=.*/UUID=$uuid/" /etc/sysconfig/network-scripts/ifcfg-$iface
+
+# Replace boot parition disk with UUID in fstab
+disk=$(grep boot /etc/fstab | awk '{print $1}')
+uuid=$(blkid $disk -o export | grep ^UUID)
+cp -f /etc/fstab /etc/fstab.disk
+sed -r "/boot/ s|$disk|$uuid|" /etc/fstab.disk > /etc/fstab
+
+EOF
+
+# To get IPs of hosts without logging in from virt-manager
 cat << 'EOF' > get_ip.exp
 #!/usr/bin/expect -f
 proc slurp {file} {
@@ -48,18 +77,22 @@ send "^]\n"
 exit 0
 EOF
 
-chmod +x startup.sh get_ip.exp
+chmod +x startup.sh get_ip.exp change_uuid.sh
 
 # A base VM with all required packages and SSH-Keys installed (if needed) should exist and shut off
-# rhel7-34, 7, 25G
 base_vm=$1
-num_disks=$2
-size=$3
-prefix=$4
+vm_prefix=$2
+num_vms=$3
+num_disks=$4
+disk_size=$5
 
-# For RHEL8 base image on RHEL7 KVM
-if [[ $base_vm =~ rhel8 ]]; then
-    cd /gluster
+# RHEL8 base on RHEL7 KVM conflicts with XFS file system forward compatibility
+host_ver=$(awk -F':' '{print $5}' /etc/system-release-cpe)
+guest_ver=$(echo $(virt-cat -d $base_vm /etc/system-release-cpe) | awk -F':' '{print $5}')
+# Pool path
+pool=$(virsh pool-dumpxml default | grep -Po '(?<=path>)[[:alnum:]/.-]+(?=<)')
+if [[ $host_ver =~ ^7 && $guest_ver =~ ^8 ]]; then
+    cd $pool
     if [ ! -d appliance ]; then
         curl -OL https://download.libguestfs.org/binaries/appliance/appliance-1.40.1.tar.xz
         tar xvfJ appliance-1.40.1.tar.xz
@@ -81,37 +114,32 @@ do
     counter=`expr $counter + 1`
 done
 
-for new_vm in {1..6}
+for new_vm in `seq 1 $num_vms`
 do
+    name="${vm_prefix}${new_vm}"
 
-    for server in {$prefix-s,$prefix-gs}
+    # Clone
+    virt-clone --original $base_vm --name $name --auto-clone
+
+    # Prep the clone
+    # TODO: Seems firstboot isn't running, better run after login `sh /usr/lib/virt-sysprep/firsboot.sh start`
+    virt-sysprep -d $name --enable $ops --firstboot ./change_uuid.sh
+
+    # Set VM autostart
+    virsh autostart $name
+
+    # Start VM and attach disks
+    virsh start $name
+
+    # Dump the info of the machine
+    virsh dominfo $name
+
+    # Create and attach disks
+    for i in `seq 1 $num_disks`
     do
-        name="${server}${new_vm}"
-
-        # Clone
-        virt-clone --original $base_vm --name $name --auto-clone
-
-        # Prep the clone
-        virt-sysprep -d $name --enable $ops --firstboot-command ./startup.sh
-
-        # Set VM autostart
-        virsh autostart $name
-
-        # Start the VM
-        virsh start $name
-
-        # Dump the info of the machine
-        virsh dominfo $name
-
-        # Create and attach disks
-        for i in `seq 1 $num_disks`
-        do
-            qemu-img create -o preallocation=metadata -f qcow2 /gluster/"$name-$i" $size
-            sleep .2
-            virsh attach-disk $name --source /gluster/"$name-$i" --target vd${dict[$i]} --driver qemu --subdriver qcow2 --persistent
-        done
-
-
+        qemu-img create -o preallocation=metadata -f qcow2 $pool/"$name-$i" $disk_size
+        sleep .2
+        virsh attach-disk $name --source $pool/"$name-$i" --target vd${dict[$i]} --driver qemu --subdriver qcow2 --persistent
     done
 done
 
@@ -123,4 +151,5 @@ done
 # mv -f hosts hostsfile
 
 # Extra
-# for i in `virsh list --all | grep -vP 'rhel|Name|--' | awk '{print $2}'`; do virsh destroy $i; virsh undefine $i; rm -f /gluster/$i*; done;
+# pool=$(virsh pool-dumpxml default | grep -Po '(?<=path>)[[:alnum:]/.-]+(?=<)')
+# for i in `virsh list --all | grep -vP 'rhel|Name|--' | awk '{print $2}'`; do virsh destroy $i; virsh undefine $i; rm -f $pool/$i*; done;
