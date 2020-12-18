b54cbd7b1e647e798c0e05937984a2ee2c1a48d9
Leela Venkaiah G <lgangava@redhat.com>
2020-12-18 20:13:22 +0530

    get IPs from newly created VMs in KVM

--- adhoc/clone-vm.sh
+++ adhoc/clone-vm.sh
@@ -5,18 +5,22 @@
 # END - Create VMs from base image and write IPs to hostsfile
 
 # $1=base_vm; $2=vm_prefix; $3=num_vms; $4=num_disks; $5=disk_size
-# ./clone-vm.sh rhel7-34 7-34 6 7 25G
+# ./clone-vm.sh centos7-34 7-34 6 7 25G
 
 # Required RPMs on KVM :
 # yum install virt libvirt libvirt-python libvirt-client libguestfs libguestfs-tools
 # yum groupinstall virtualization-client virtualization-platform virtualization-tools
 # yum expect
 
-# IMP: Run below `startup.sh` in the base image before cloning
+# Make sure you create a file named `password.txt` in script's directory with plain password of base VM as the content (manual)
+[ ! -f password.txt ] && echo "No password.txt file exists in current directory" && exit -1
+
+# IMP: Run below `startup.sh` in the base image before cloning (manual)
+# Copy SSH key too for easy login into new VMs
 cat << 'EOF' > startup.sh
 #!/bin/bash
 
-# Enalble console access for every VM
+# Enable console access for every VM
 systemctl enable serial-getty@ttyS0.service
 systemctl start serial-getty@ttyS0.service
 
@@ -33,7 +37,7 @@ sed -r "/boot/ s|^UUID=[[:xdigit:]-]+|$disk|" /etc/fstab.uuid > /etc/fstab
 
 EOF
 
-# Script to be run when VMs are booting for the first time
+# Script to be run when VMs are booting for the first time (automatic)
 cat << 'EOF' > change_uuid.sh
 #!/bin/bash
 
@@ -51,9 +55,10 @@ sed -r "/boot/ s|$disk|$uuid|" /etc/fstab.disk > /etc/fstab
 
 EOF
 
-# To get IPs of hosts without logging in from virt-manager
+# To get IPs of hosts without logging in from virt-manager (automatic)
 cat << 'EOF' > get_ip.exp
 #!/usr/bin/expect -f
+set timeout 5
 proc slurp {file} {
     set fh [open $file r]
     set ret [read $fh]
@@ -71,7 +76,7 @@ send $password
 expect "#"
 send "hostname -I\n"
 expect "#"
-send "logout\n"
+send "exit\n"
 expect "login:"
 send "^]\n"
 exit 0
@@ -86,13 +91,14 @@ num_vms=$3
 num_disks=$4
 disk_size=$5
 
-# RHEL8 base on RHEL7 KVM conflicts with XFS file system forward compatibility
+# centos8 base on centos7 KVM conflicts with XFS file system forward compatibility
 host_ver=$(awk -F':' '{print $5}' /etc/system-release-cpe)
 guest_ver=$(echo $(virt-cat -d $base_vm /etc/system-release-cpe) | awk -F':' '{print $5}')
-# Pool path
-pool=$(virsh pool-dumpxml default | grep -Po '(?<=path>)[[:alnum:]/.-]+(?=<)')
+# Pool path, (if not using default path, it should be change below accordingly or
+# for any complex path directly declare it i.e, pool_path=<COMPLEX_PATH>)
+pool_path=$(virsh pool-dumpxml default | grep -Po '(?<=path>)[[:alnum:]/.-]+(?=<)')
 if [[ $host_ver =~ ^7 && $guest_ver =~ ^8 ]]; then
-    cd $pool
+    cd $pool_path
     if [ ! -d appliance ]; then
         curl -OL https://download.libguestfs.org/binaries/appliance/appliance-1.40.1.tar.xz
         tar xvfJ appliance-1.40.1.tar.xz
@@ -114,16 +120,20 @@ do
     counter=`expr $counter + 1`
 done
 
-for new_vm in `seq 1 $num_vms`
+vm_names=()
+for i in `seq 1 $num_vms`
 do
-    name="${vm_prefix}${new_vm}"
+    vm_names+=("${vm_prefix}${i}")
+done
 
+for name in ${vm_names[@]}
+do
     # Clone
     virt-clone --original $base_vm --name $name --auto-clone
 
     # Prep the clone
-    # TODO: Seems firstboot isn't running, better run after login `sh /usr/lib/virt-sysprep/firsboot.sh start`
-    virt-sysprep -d $name --enable $ops --firstboot ./change_uuid.sh
+    # if `--selinux-relabel` is removed then `firshboot` script will not be trigerred
+    virt-sysprep -d $name --selinux-relabel --enable $ops --firstboot ./change_uuid.sh
 
     # Set VM autostart
     virsh autostart $name
@@ -131,25 +141,30 @@ do
     # Start VM and attach disks
     virsh start $name
 
-    # Dump the info of the machine
-    virsh dominfo $name
-
-    # Create and attach disks
+    Create and attach disks
     for i in `seq 1 $num_disks`
     do
-        qemu-img create -o preallocation=metadata -f qcow2 $pool/"$name-$i" $disk_size
+        qemu-img create -o preallocation=metadata -f qcow2 $pool_path/"$name-$i" $disk_size
         sleep .2
-        virsh attach-disk $name --source $pool/"$name-$i" --target vd${dict[$i]} --driver qemu --subdriver qcow2 --persistent
+        virsh attach-disk $name --source $pool_path/"$name-$i" --target vd${dict[$i]} --driver qemu --subdriver qcow2 --persistent
     done
 done
 
-# Get IPs of VMs and write to hosts file
-# Run below after all VMs are up and running
-# echo "# VMs from base image $base_image" > hostsfile
-# for name in `virsh list --all | grep -vP 'rhel|Name|--' | awk '{print $2}'`; do ./get_ip.exp $name | sed -n '/hostname/{n;p;}' | awk -v name=$name '{gsub("-","",name); print $1, name}' >> hostsfile; done;
-# sort -t' ' -k2 hostsfile > hosts
-# mv -f hosts hostsfile
+
+echo "Writing IP addresses of newly created VMs to 'hostsfile'"
+echo "# VMs from base VM $base_vm" > hostsfile
+for name in ${vm_names[@]}
+do
+    # Write IP in hostsfiles
+    count=1
+    while ! grep -q ${name/-/} hostsfile
+    do
+        expect get_ip.exp $name  | grep -A 1 hostname | grep -Po '\d+\.\d+\.\d+\.\d+' | awk -v name=$name '{gsub("-","",name); print $1, name}' >> hostsfile;
+        if [ $count -eq 2 ]; then break; fi;
+        ((count++))
+    done
+done
 
 # Extra
-# pool=$(virsh pool-dumpxml default | grep -Po '(?<=path>)[[:alnum:]/.-]+(?=<)')
-# for i in `virsh list --all | grep -vP 'rhel|Name|--' | awk '{print $2}'`; do virsh destroy $i; virsh undefine $i; rm -f $pool/$i*; done;
+# pool_path=$(virsh pool-dumpxml default | grep -Po '(?<=path>)[[:alnum:]/.-]+(?=<)')
+# for i in `virsh list --all | grep -vP 'centos|Name|leela|--' | awk '{print $2}'`; do virsh destroy $i; virsh undefine $i; rm -f $pool_path/$i*; done;
