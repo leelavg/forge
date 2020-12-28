9054f0b6449063cf305c5665e42032af752c1856
Leela Venkaiah G <lgangava@redhat.com>
2020-12-28 12:31:10 +0530

    Add minimal documentation, error handling

--- adhoc/clone-vm.sh
+++ adhoc/clone-vm.sh
@@ -1,19 +1,54 @@
 #!/bin/bash
 
-# Please change paths according to your setup
-# BEGIN - Existing base image
-# END - Create VMs from base image and write IPs to hostsfile
-
+# Usage:
 # $1=base_vm; $2=vm_prefix; $3=num_vms; $4=num_disks; $5=disk_size
 # ./clone-vm.sh centos7-34 7-34 6 7 25G
+
+# BEGIN - Existing base image
+# END - Create VMs from base image, attach disks and write IPs to hostsfile
 
 # Required RPMs on KVM :
 # yum install virt libvirt libvirt-python libvirt-client libguestfs libguestfs-tools
 # yum groupinstall virtualization-client virtualization-platform virtualization-tools
 # yum expect
 
+# Additional Notes:
+#
+# Scope:
+# - Clone a base VM (either ISO or PXE booted), attach disks and take note of it's IP
+# - Thin base image results in faster cloning and prepping the clones
+# - Minimum expectation from the script is:
+#   - A matching base VM name as supplied in the arguments
+#   - `startup.sh` (refer below) script is run on the base VM and presence of `password.txt` in `pwd`
+#   - Shutdown the base VM after installing/tuning required (like running `startup.sh`) settings
+#   - Run the script with necessary arguments as stated in `Usage:`
+#
+# Cases covered in addition to cloning VM and attaching disks:
+# - Force re-label SELinux labels
+# - Support for cloning CentOS 8 base VM on CentOS 7 KVM (similar for RHEL)
+# - Custom scripts on replacing correct UUIDs of disks and network interfaces
+# - No deletion of VMs is happening and KVM by defualt prohibits overwrites of domain
+#
+# Known limitations:
+# - Only core functionality (clone VM, attach disks, take note of IP) is implemented
+# - Argument parsing, checking for available space isn't implemented
+# - No error recovery is inbuilt (just backoff if something goes wrong)
+# - Password of base VM isn't encrypted and not checked for correctness
+
+# Break on any failure
+set -e
+set -o pipefail
+
 # Make sure you create a file named `password.txt` in script's directory with plain password of base VM as the content (manual)
 [ ! -f password.txt ] && echo "No password.txt file exists in current directory" && exit -1
+base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+temp_dir=$(mktemp -d)
+
+# Delete temporary directory on script exit
+trap "rm -rf $temp_dir" EXIT
+
+cp password.txt $temp_dir
+cd $temp_dir
 
 # IMP: Run below `startup.sh` in the base image before cloning (manual)
 # Copy SSH key too for easy login into new VMs
@@ -31,7 +66,7 @@ sed -i 's/ONBOOT=no/ONBOOT=yes/' /etc/sysconfig/network-scripts/ifcfg-$iface
 
 # virt-sysprep is still leaving some UUIDs
 # Replace UUID of boot partition with actual disk name
-disk=$(df -k /boot --output=source | tail -n 1)
+disk=$(df /boot --output=source | tail -n 1)
 cp -f /etc/fstab /etc/fstab.uuid
 sed -r "/boot/ s|^UUID=[[:xdigit:]-]+|$disk|" /etc/fstab.uuid > /etc/fstab
 
@@ -94,7 +129,8 @@ disk_size=$5
 # centos8 base on centos7 KVM conflicts with XFS file system forward compatibility
 host_ver=$(awk -F':' '{print $5}' /etc/system-release-cpe)
 guest_ver=$(echo $(virt-cat -d $base_vm /etc/system-release-cpe) | awk -F':' '{print $5}')
-# Pool path, (if not using default path, it should be change below accordingly or
+
+# Pool path, (if not using default path, it should be changed in below regex accordingly or
 # for any complex path directly declare it i.e, pool_path=<COMPLEX_PATH>)
 pool_path=$(virsh pool-dumpxml default | grep -Po '(?<=path>)[[:alnum:]/.-]+(?=<)')
 if [[ $host_ver =~ ^7 && $guest_ver =~ ^8 ]]; then
@@ -104,10 +140,10 @@ if [[ $host_ver =~ ^7 && $guest_ver =~ ^8 ]]; then
         tar xvfJ appliance-1.40.1.tar.xz
     fi
     export LIBGUESTFS_PATH=`pwd`/appliance/
-    cd -
+    cd $temp_dir
 fi
 
-# Options that needs to be enabled
+# Options that needs to be enabled, preserve SSH related info
 ops=$(virt-sysprep --list-operations | grep -Pv 'ssh-userdir|ssh-hostkeys' | awk '{ printf "%s,", $1}' | sed 's/,$//')
 
 declare -A dict
@@ -123,7 +159,7 @@ done
 vm_names=()
 for i in `seq 1 $num_vms`
 do
-    vm_names+=("${vm_prefix}${i}")
+    vm_names+=("${vm_prefix}-s${i}")
 done
 
 for name in ${vm_names[@]}
@@ -141,7 +177,7 @@ do
     # Start VM and attach disks
     virsh start $name
 
-    Create and attach disks
+    # Create and attach disks
     for i in `seq 1 $num_disks`
     do
         qemu-img create -o preallocation=metadata -f qcow2 $pool_path/"$name-$i" $disk_size
@@ -155,7 +191,7 @@ echo "Writing IP addresses of newly created VMs to 'hostsfile'"
 echo "# VMs from base VM $base_vm" > hostsfile
 for name in ${vm_names[@]}
 do
-    # Write IP in hostsfiles
+    # Write IP in hostsfile
     count=1
     while ! grep -q ${name/-/} hostsfile
     do
@@ -165,6 +201,13 @@ do
     done
 done
 
+# Copy created hostsfile
+cd $base_dir
+cp -f $temp_dir/hostsfile .
+echo "VMs are created and IPs are captured in hostsfile"
+echo "Complete!"
+
 # Extra
 # pool_path=$(virsh pool-dumpxml default | grep -Po '(?<=path>)[[:alnum:]/.-]+(?=<)')
 # for i in `virsh list --all | grep -vP 'centos|Name|leela|--' | awk '{print $2}'`; do virsh destroy $i; virsh undefine $i; rm -f $pool_path/$i*; done;
+
